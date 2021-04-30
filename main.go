452c3414509466bbb7b3f479ab0571eaf1e25c5e
Tamal Saha <tamal@appscode.com>
2021-04-30 09:58:23 -0700

    added-all

--- main.go
+++ main.go
@@ -5,39 +5,66 @@ import (
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"os"
 )
 
 func main() {
 	sel, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
 		MatchLabels: map[string]string{
-			"app": "nginx",
+			"app":  "nginx",
+			"tier": "frontend",
 		},
-		MatchExpressions: nil,
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      "node/region",
+				Operator: metav1.LabelSelectorOpIn,
+				Values:   []string{"us-east-1a", "us-east-1b"},
+			},
+		},
 	})
 	if err != nil {
 		panic(err)
 	}
 	reqs := SelectorToArray(sel)
-	fmt.Println(reqs)
+	printJSON(reqs)
 	fmt.Println(sel)
+
+	fmt.Println("Everything =================")
+
+	selEverything := labels.Everything()
+	reqsEverything := SelectorToArray(selEverything)
+	printJSON(reqsEverything)
+	fmt.Println(selEverything)
+
+	fmt.Println("Nothing =================")
+
+	selNothing := labels.Nothing()
+	reqsNothing := SelectorToArray(selNothing)
+	printJSON(reqsNothing)
+	fmt.Println(selNothing)
 }
 
 func printJSON(v interface{}) {
-	data, err := json.MarshalIndent(v, "", "  ")
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetEscapeHTML(false)
+	encoder.SetIndent("", "  ")
+	err := encoder.Encode(v)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(data))
 }
 
 func SelectorToArray(sel labels.Selector) []string {
 	requirements, selectable := sel.Requirements()
 	if !selectable {
-		fmt.Printf("sel= %v is NOT selectable", sel)
+		return []string{"<NONE>"}
 	}
 	reqs := make([]string, 0, len(requirements))
 	for _, r := range requirements {
 		reqs = append(reqs, r.String())
+	}
+	if len(reqs) == 0 {
+		reqs = []string{"<ALL>"}
 	}
 	return reqs
 }
